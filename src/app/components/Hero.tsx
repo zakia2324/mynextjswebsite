86ffdbf08c93dd54d390feb2e69aba07d9fbae54
zakia2324 <160940055+zakia2324@users.noreply.github.com>
2025-05-14 04:19:13 +0500

    new changes

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -8,10 +8,12 @@ const Hero = () => {
       <section className="text-gray-600 body-font bg-gray-100 pt-[60px]">
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+      <h1 className="title-font sm:text-4xl text-3xl mb-3 font-medium text-gray-900">
         Hello! I am a frontend
         <br className="hidden lg:inline-block" />
-        develpor
+        <br/>
+        developer
+        <br className="hidden lg:inline-block" />
       </h1>
       <p className="mb-8 leading-relaxed ">
       I am a passionate frontend developer with expertise in modern web technologies. My skills set includes HTML, CSS, JavaScript, TypeScript, Next.js, Tailwind CSS, and ShadCN. I specialize in creating aesthetically pleasing and functional interfaces that prioritize responsiveness and user experience. My approach goes beyond writing code—I strive to craft seamless, intuitive, and engaging digital experiences.
